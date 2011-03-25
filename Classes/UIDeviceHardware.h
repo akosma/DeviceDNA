ed7c34ce8ae14ec131fdf55d7a2dc746803e824d
Adrian Kosmaczewski <adrian@akosma.com>
2011-03-25 11:01:22 +0100

    Added a new class providing more information about the current device; adapted the project to Xcode 4

---
+++ Classes/UIDeviceHardware.h
@@ -0,0 +1,16 @@
+//
+//  UIDeviceHardware.h
+//  DeviceDNA
+//
+//  Created by Adrian on 3/25/11.
+//  Copyright 2011 akosma software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface UIDeviceHardware : NSObject {
+    
+}
+
+@end
---
+++ Classes/UIDeviceHardware.m
@@ -0,0 +1,14 @@
+//
+//  UIDeviceHardware.m
+//  DeviceDNA
+//
+//  Created by Adrian on 3/25/11.
+//  Copyright 2011 akosma software. All rights reserved.
+//
+
+#import "UIDeviceHardware.h"
+
+
+@implementation UIDeviceHardware
+
+@end
